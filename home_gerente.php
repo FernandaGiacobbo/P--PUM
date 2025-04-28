36038759000238e9b5acfb5e8004e7dab687218f
Sofhia Chromiec <126419824+SofhiaC@users.noreply.github.com>
2025-04-28 17:46:31 -0300

    gerenciamento

---
+++ home_gerente.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+    if(!isses($_SESSION['id'])) {
+        header('Location: index.php');
+        exit();
+    }
+
+    include 'conecta_db.php';
+
+    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
+        header('location: index.php');
+     } 
+
+    include_once('conecta_db.php');
+    $oMysql = conecta_db();
+
+    $logado = $_SESSION['nome'];
+    $id_us = $_SESSION['id']; 
+
+    $query = "SELECT * from tb_usuario WHERE id_usuario = '$id_us'";
+    $resultado = $oMysql->query($query);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel</title>
+</head>
+<body>
+    
+</body>
+</html>
