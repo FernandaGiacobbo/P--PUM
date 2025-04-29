0234fa7ce885c33edba3d3e288bf3a6e135572b1
Sofhia Chromiec <126419824+SofhiaC@users.noreply.github.com>
2025-04-28 21:23:12 -0300

    dayli com a data ajustada

--- criarDayli.php
+++ criarDayli.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+date_default_timezone_set('America/Sao_Paulo');
 
 $id_us = $_SESSION['id'];
 
@@ -8,7 +9,9 @@ if(isset($_POST['submit'])){
     include_once('conecta_db.php');
     $oMysql = conecta_db();
 
-    $query2 = "SELECT data_daily, id_daily from tb_daily WHERE usuario_id = $id_us";
+    $data = date('Y-m-d');
+    $query2 = "SELECT data_daily, id_daily FROM tb_daily WHERE usuario_id = $id_us AND data_daily = '$data'";
+
     $resultado2 = $oMysql->query($query2);
 
     if ($res = mysqli_fetch_assoc($resultado2)) {
@@ -16,6 +19,7 @@ if(isset($_POST['submit'])){
         $id_day = $res['id_daily'];
         echo "<script>alert('Parece que você já criou um daily hoje: ');</script>";
         echo "<script>window.location.href = 'Dailyeditar.php?id_daily=$id_day';</script>";
+        
         die();
     } else {
         
@@ -26,8 +30,6 @@ if(isset($_POST['submit'])){
 
         $query = "INSERT INTO tb_daily(data_daily, mooday_daily, texto_daily, usuario_id) VALUES ('$data', '$mood', '$rotina', '$id_us')";
         $resultado = $oMysql->query($query);
-
-        $id_daily = $oMysql->insert_id;
 
         header('location: dayli.php');
     }
